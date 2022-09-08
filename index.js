f9bae5ff76c96617919ad6f0fccd83f35fc5db8c
michael <michael@pop-os.localdomain>
2022-09-08 17:13:42 +0300

    index.html, index.js

--- index.js
+++ index.js
@@ -3,17 +3,35 @@ const options = {
 	method: 'GET',
 	headers: {
 		'X-RapidAPI-Key': '22f844f865msh981957b4874470fp1a7235jsn8251ca897c41',
-		'X-RapidAPI-Host': 'moviesdb5.p.rapidapi.com'
+		'X-RapidAPI-Host': 'movie-database-alternative.p.rapidapi.com'
 	}
 };
 
-fetch('https://moviesdb5.p.rapidapi.com/om?t=Game%20of%20Thrones', options)
+fetch('https://movie-database-alternative.p.rapidapi.com/?s=Avengers%20Endgame&r=json&page=1', options)
 	.then(response => response.json())
-	.then(data => console.log(data))
+	.then(data => movieData(data))
 	.catch(err => console.error(err));
+
+// hoisting variable globally
+const apiContainer = document.getElementById("apiContainer");
+const image = document.getElementById("imageCard")
+
+
+// getting the title of the movie the user is searching for
+document.getElementById("submitForm").addEventListener("submit",
+function getValue(title){
+	title.preventDefault();
+	const movieTitle = document.querySelector("input").value
+	console.log (movieTitle)
+})
+
+
 
 
 // function to fetch the API information
-function data (){
-
-}
+// function movieData (data){
+// 	 image.src = data.Poster
+//     //  const title  = data.title;
+// 	//  const plot = data.plot
+// 	 console.log (`${poster}`)
+// }
